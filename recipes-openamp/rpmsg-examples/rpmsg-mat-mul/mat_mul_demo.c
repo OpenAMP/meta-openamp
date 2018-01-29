dbdcfb61ebaaf1f07dcf8ffb842100c08d18950e
Wendy Liang <jliang@xilinx.com>
2018-01-29 10:54:31 -0800

    rpmsg matrix multiplication to support rpmsg char
    Add RPMsg char driver support to RPMsg matrix multiplication
    demo.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- recipes-openamp/rpmsg-examples/rpmsg-mat-mul/mat_mul_demo.c
+++ recipes-openamp/rpmsg-examples/rpmsg-mat-mul/mat_mul_demo.c
@@ -15,6 +15,7 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include <string.h>
+#include <linux/rpmsg.h>
 
 #define MATRIX_SIZE 6
 
@@ -71,7 +72,7 @@ static void generate_matrices(int num_matrices,
 static pthread_t ui_thread, compute_thread;
 static pthread_mutex_t sync_lock;
 
-static int fd, compute_flag;
+static int charfd = -1, fd, compute_flag;
 static int ntimes = 1;
 static struct _matrix i_matrix[2];
 static struct _matrix r_matrix;
@@ -142,11 +143,59 @@ void *compute_thread_entry(void *ptr)
 	return 0;
 }
 
+int rpmsg_create_ept(int rpfd, struct rpmsg_endpoint_info *eptinfo)
+{
+	int ret;
+
+	ret = ioctl(rpfd, RPMSG_CREATE_EPT_IOCTL, eptinfo);
+	if (ret)
+		perror("Failed to create endpoint.\n");
+	return ret;
+}
+
+char *get_rpmsg_ept_dev_name(char *rpmsg_char_name, char *ept_name,
+			     char *ept_dev_name)
+{
+	char sys_rpmsg_ept_name_path[64];
+	char svc_name[64];
+	char *sys_rpmsg_path = "/sys/class/rpmsg";
+	FILE *fp;
+	int i;
+	int ept_name_len;
+
+	for (i = 0; i < 128; i++) {
+		sprintf(sys_rpmsg_ept_name_path, "%s/%s/rpmsg%d/name",
+			sys_rpmsg_path, rpmsg_char_name, i);
+		printf("checking %s\n", sys_rpmsg_ept_name_path);
+		fp = fopen(sys_rpmsg_ept_name_path, "r");
+		if (!fp) {
+			printf("failed to open %s\n", sys_rpmsg_ept_name_path);
+			break;
+		}
+		fgets(svc_name, sizeof(svc_name), fp);
+		fclose(fp);
+		printf("svc_name: %s.\n",svc_name);
+		ept_name_len = strlen(ept_name);
+		if (ept_name_len > sizeof(svc_name))
+			ept_name_len = sizeof(svc_name);
+		if (!strncmp(svc_name, ept_name, ept_name_len)) {
+			sprintf(ept_dev_name, "rpmsg%d", i);
+			return ept_dev_name;
+		}
+	}
+
+	printf("Not able to RPMsg endpoint file for %s:%s.\n",
+	       rpmsg_char_name, ept_name);
+	return NULL;
+}
+
 int main(int argc, char *argv[])
 {
 	unsigned int size;
 	int opt;
 	char *rpmsg_dev="/dev/rpmsg0";
+	char *rpmsg_char_name;
+	struct rpmsg_endpoint_info eptinfo;
 
 	while ((opt = getopt(argc, argv, "d:n:")) != -1) {
 		switch (opt) {
@@ -171,6 +220,34 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	rpmsg_char_name = strstr(rpmsg_dev, "rpmsg_ctrl");
+	if (rpmsg_char_name != NULL) {
+		char ept_dev_name[16];
+		char ept_dev_path[32];
+		int ret;
+
+		strcpy(eptinfo.name, "rpmsg-openamp-demo-channel");
+		eptinfo.src = 0;
+		eptinfo.dst = 0xFFFFFFFF;
+		ret = rpmsg_create_ept(fd, &eptinfo);
+		if (ret) {
+			printf("failed to create RPMsg endpoint.\n");
+			return -1;
+		}
+		charfd = fd;
+
+		if (!get_rpmsg_ept_dev_name(rpmsg_char_name, eptinfo.name,
+					   ept_dev_name))
+			return -1;
+		sprintf(ept_dev_path, "/dev/%s", ept_dev_name);
+		fd = open(ept_dev_path, O_RDWR | O_NONBLOCK);
+		if (fd < 0) {
+			perror("Failed to open rpmsg device.");
+			close(charfd);
+			return -1;
+		}
+	}
+
 	if (pthread_mutex_init(&sync_lock, NULL) != 0)
 		printf("\r\n mutex initialization failure \r\n");
 
@@ -187,6 +264,8 @@ int main(int argc, char *argv[])
 	pthread_join(compute_thread, NULL);
 
 	close(fd);
+	if (charfd >= 0)
+		close(charfd);
 
 	printf("\r\n Quitting application .. \r\n");
 	printf(" Matrix multiply application end \r\n");
